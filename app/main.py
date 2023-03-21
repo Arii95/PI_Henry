4309d317e69a289aa29b0cb27a474f090523e07b
Arii95 <86978389+Arii95@users.noreply.github.com>
2023-03-20 23:50:23 -0600

    avance modelo

--- app/main.py
+++ app/main.py
@@ -17,12 +17,10 @@ async def startup():
 #Obtener el título de la película con mayor duración, dado año, plataforma y tipo de duración
 @app.get("/get_max_duration/{year}/{plataforma}/{duration}")
 async def get_max_duration(year:int, plataforma:str,duration:str):
-    print(type(year),type(plataforma))
     rslt_df = df_pr.loc[(df_pr['Year'] == year) &
               (df_pr['plataforma']==plataforma)&(df_pr['duration_type']==duration)]
     max_value=int(rslt_df['duration_int'].max())
     fila_max_valor = rslt_df.loc[rslt_df['duration_int'].eq(max_value)]
-    print(rslt_df['duration_int'].dtype)
     a= fila_max_valor['title'].values
     return f'la pelicula es: {a}'
 
@@ -49,7 +47,6 @@ async def get_actor(plataforma:str,year:int):
     df_pr['cast'].fillna(' ',inplace= True)
     rslt_df = df_pr.loc[(df_pr['Year'] == year) & (df_pr['plataforma']==plataforma)]
     tolist= list(rslt_df['cast'].str.split(', '))
-    print(tolist)
     lista_completa = []
     for listas in tolist:
         for elemento in listas:
